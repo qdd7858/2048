36a463c96c264203281fe79c0a590e8df9af49cb
qdd7858 <qdd7858@g.rit.edu>
2020-06-15 20:35:53 -0400

    Add animation to the GUI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.animation.ScaleTransition;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -12,6 +13,7 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import model.Board;
 import model.Game;
 
@@ -29,25 +31,29 @@ public class Main extends Application{
         Game game = new Game();
         Board board = game.getBoard();
         GridPane layout = new GridPane();
-        Scene scene = new Scene(layout, 400, 400);
+        Scene scene = new Scene(layout, 470, 470);
         this.stage = stage;
         this.scene = scene;
         this.layout = layout;
         this.game = game;
         this.board = board;
         this.nodeList = new TextField[Board.ROW_INDEX][Board.COL_INDEX];
+        layout.setPadding(new Insets(20,20,20,20));
+        layout.setHgap(10);
+        layout.setVgap(10);
         for (int row = 0; row < Board.ROW_INDEX; row++){
             for (int col = 0; col < Board.COL_INDEX; col++){
                 TextField textField = new TextField();
                 textField.setMinSize(100,100);
                 textField.setText(Integer.toString(board.getValueAt(row, col)));
                 textField.setAlignment(Pos.CENTER);
-                textField.setStyle("-fx-text-inner-color: silver;");
+                textField.setStyle("-fx-text-inner-color: white;");
                 textField.setEditable(false);
                 textField.setMouseTransparent(true);
                 textField.setFocusTraversable(false);
                 layout.add(textField, col, row);
                 nodeList[row][col] = textField;
+                setTextFieldColor(row,col);
             }
         }
         stage.setScene(scene);
@@ -81,8 +87,17 @@ public class Main extends Application{
         game.randGenererate();
         for (int row = 0; row < Board.ROW_INDEX; row++) {
             for (int col = 0; col < Board.COL_INDEX; col++) {
+                if (Integer.parseInt(nodeList[row][col].getCharacters().toString()) < board.getValueAt(row, col)){
+                    ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(0.03), nodeList[row][col]);
+                    scaleTransition.setCycleCount(2);
+                    scaleTransition.setAutoReverse(true);
+                    scaleTransition.setToX(1.1);
+                    scaleTransition.setToY(1.1);
+                    scaleTransition.play();
+                }
                 nodeList[row][col].setText(Integer.toString(board.getValueAt(row, col)));
                 setTextFieldColor(row,col);
+
             }
         }
 
